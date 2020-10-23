eb27ac7ba55cdd3cc95a6c13ecfdf7ec6bf7f293
gjones81 <jones.guy81@gmail.com>
2020-10-22 17:07:12 -0700

    accounts app in progress, needs urls and migrations

--- SimpleSocial/accounts/models.py
+++ SimpleSocial/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib import auth
 
 # Create your models here.
+class User(auth.models.User, auth.models.PermissionsMixin):
+
+    def __str__(self):
+        # .username is builtin to the User class from models
+        return "@{}".format(self.username)
